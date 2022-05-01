7aa16fb0e5add25c50c068cd3a7c83402f9adf9c
maiyama <myam0118@gmail.com>
2022-05-01 18:10:17 +0900

    NSPersistentCloudKitContainer を AppDelegate から分離

---
+++ TamaParks/PersistentProvider.swift
@@ -0,0 +1,33 @@
+//
+//  PersistentProvider.swift
+//  TamaParks
+//
+//  Created by maiyama on 2022/05/01.
+//
+
+import CoreData
+
+final class PersistentProvider {
+    static let shared: PersistentProvider = .init()
+    
+    let persistentContainer: NSPersistentCloudKitContainer
+    
+    private init() {
+        persistentContainer = NSPersistentCloudKitContainer(name: "TamaParks")
+        
+        let description = NSPersistentStoreDescription()
+        description.cloudKitContainerOptions = NSPersistentCloudKitContainerOptions(containerIdentifier: "iCloud.com.muijp.TamaParks")
+        description.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
+        description.setOption(true as NSNumber, forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
+        
+        persistentContainer.persistentStoreDescriptions = [description]
+        
+        persistentContainer.viewContext.automaticallyMergesChangesFromParent = true
+        
+        persistentContainer.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("failed to load CoreData store: \(error)")
+            }
+        }
+    }
+}
